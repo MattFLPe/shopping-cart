c41751bf5ab32d1498024da78a4181f3e4606f2e
Mateus Felipe <brasil.mateus748@gmail.com>
2024-08-27 06:08:52 -0300

    Add Shop Page component

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar.js";
+
+const Shop = () => {
+    return (
+        <>
+        <p>AAAA</p>
+        
+
+        </>
+    
+    )
+}
+
+export default Shop;
