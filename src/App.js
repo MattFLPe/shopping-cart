ed1ff9ebcae30bbcf1a06a52a1d2d4f7f34dd6d2
Mateus Felipe <brasil.mateus748@gmail.com>
2024-08-27 06:03:31 -0300

    Add imports and the routes of the navbar router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar/Navbar.js'
+import Home from './components/Home/Home.js';
+import Shop from './components/Shop/Shop.js';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+          <Routes>        
+            <Route path="/Home"  element={<Home />} />
+            <Route path="/Shop"  element={<Shop />}/>
+          </Routes>
     </div>
   );
 }
