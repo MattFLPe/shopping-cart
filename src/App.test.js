4ff5481dc6af4691fdeab7f301d5935d26a15697
Mateus Felipe <brasil.mateus748@gmail.com>
2024-08-28 12:54:34 -0300

    Create test suite for App.js with some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import '@testing-library/jest-dom';
+
+describe('App Component', () => {
+
+  it('should render the Navbar', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+    const navbarElement = screen.getByRole('navigation');
+    expect(navbarElement).toBeInTheDocument();
+  });
+
+  it('should render Home component on default route', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+    const homeHeadline = screen.getByText('GET YOUR FIRST CAR HERE!');
+    expect(homeHeadline).toBeInTheDocument();
+  });
+
+  it('should render Shop component on /Shop route', () => {
+    render(
+      <MemoryRouter initialEntries={['/Shop']}>
+        <App />
+      </MemoryRouter>
+    );
+    const shopComponentText = screen.getByText('Shop Page'); // Assuming there's some text in your Shop component
+    expect(shopComponentText).toBeInTheDocument();
+  });
+
 });
