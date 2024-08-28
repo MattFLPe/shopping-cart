50b1bd5489d4176b857733778fc62041b07665e3
Mateus Felipe <brasil.mateus748@gmail.com>
2024-08-28 13:06:27 -0300

    Add new JSX elements with styles from CSS modules

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React from "react"
 import styles from "./Home.module.css"
-import car from "./car.jpg"
+import benz from "./assets/benz.jpg"
+import blueaudi from "./assets/blueaudi.jpg"
+import whitebenz from "./assets/whitebenz.jpg"
+import twocars from "./assets/twocars.jpg"
 
 const Home = () => {
     return (
         <>
-        
-        <img className={styles.img} src={car} alt="" />
-           
-
+        <div className={styles.containerTitles}>
+            <h1>GET YOUR FIRST CAR HERE!</h1>
+            <h3>Go for a test drive!</h3>
+        </div>
+        <div className={styles.divHome}>
+            <img className={styles.imgHome} src={twocars} alt="" />
+        </div>
+        <div className={styles.container}>
+            <img className={styles.imgCar} src={benz} alt="" />
+            <img className={styles.imgCar} src={whitebenz} alt="" />
+            <img className={styles.imgCar} src={blueaudi} alt="" />
+        </div>
+        <footer className={styles.footer}>Images by: Mike Birdy on Pexels.</footer>
         </>
     
     )
