04d806708f6d6fbd36a30635ecb165f95b7bcf69
Mateus Felipe <brasil.mateus748@gmail.com>
2024-08-29 19:49:34 -0300

    Wrap App.js in CartProvider

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar.js'
 import Home from './components/Home/Home.js';
 import Shop from './components/Shop/Shop.js';
+import { CartProvider } from './context/CartContext';
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+    <CartProvider>
+      <div className="App">
+        <Navbar />
           <Routes>        
             <Route path="/"  element={<Home />} />
             <Route path="/Shop"  element={<Shop />}/>
           </Routes>
-    </div>
+      </div>
+    </CartProvider>
   );
 }
 
