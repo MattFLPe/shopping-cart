b6fa408d11097cf39e2462986e6438a5d99350a0
Mateus Felipe <brasil.mateus748@gmail.com>
2024-08-29 19:59:16 -0300

    Update Card component: Remove pointless img elements Add div container and the card item elements

--- src/components/Shop/Card/Card.js
+++ src/components/Shop/Card/Card.js
@@ -1,19 +1,25 @@
 import React from "react";
 import styles from "./Card.module.css"
-import luxuriouscar from "../assets/luxuriouscar.jpg"
-import luxuriouscar2 from "../assets/luxuriouscar2.jpg"
+import audi from "../assets/audi.jpg"
 
+const Card = ({ onAddToCart }) => {
+    const item = { id: 1, name: "Sample Item", price: 15.999 };
 
-const Cards = () => {
     return (
         <>
         <div className={styles.card}>
-            <img className={styles.imgLuxuriousCar} src={luxuriouscar} alt="" />
-            <img className={styles.imgLuxuriousCar} src={luxuriouscar2} alt="" />
+            <div className={styles.itemContainer}>
+                <h2>{item.name}</h2>
+                <p>${item.price}</p>
+                <img className={styles.imgLuxuriousCar} src={audi} alt="" />
+                <label>Quantity: <input></input></label>
+                <button onClick={() => onAddToCart(item)}>Add to Cart</button>
+            </div>
         </div>
         </>
     
     )
 }
 
-export default Cards;
+
+export default Card;
