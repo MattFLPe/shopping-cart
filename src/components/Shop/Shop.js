159f9e8458bdc3851dedd07be52efd5cc6f79ab3
Mateus Felipe <brasil.mateus748@gmail.com>
2024-08-29 19:57:18 -0300

    Update Shop component: Create handler to add items to the cart

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,13 +1,20 @@
 import React from "react";
 import styles from "./Shop.module.css"
 import Card from "./Card/Card.js"
+import { useCart } from "../../context/CartContext";
 
 const Shop = () => {
+    const { addToCart } = useCart();
+
+    const handleAddToCart = (item) => {
+        addToCart(item);
+    };
+
     return (
         <>
         <h1 className={styles.titleShop}>Shop Page</h1>
         <div className={styles.shopcontainer}>
-            <Card />
+            <Card onAddToCart={handleAddToCart} />
         </div>
         </>
     
