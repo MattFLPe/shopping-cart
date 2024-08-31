e7e67d28368a235881df66198eddcc01409bcb37
Mateus Felipe <brasil.mateus748@gmail.com>
2024-08-31 02:47:15 -0300

    Replace imports and src of img elements

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,26 @@
 import React from "react"
 import styles from "./Home.module.css"
-import audi from "./assets/audi.jpg"
-import clio from "./assets/clio.jpg"
-import benz2 from "./assets/benz2.jpg"
-import twocars from "./assets/twocars.jpg"
+import imgHome from "./assets/imgHome.jpg"
+import kit from "./assets/kit.jpg"
+import kit2 from "./assets/kit2.jpg"
+import kit3 from "./assets/kit3.jpg"
 
 const Home = () => {
     return (
         <>
         <div className={styles.containerTitles}>
-            <h1>GET YOUR FIRST CAR HERE!</h1>
-            <h3>Go for a test drive!</h3>
+            <h2>General Store</h2>
         </div>
         <div className={styles.divHome}>
-            <img className={styles.imgHome} src={twocars} alt="" />
+            <img className={styles.imgHome} src={imgHome} alt="" />
+            <h3 className={styles.h3}>Clothing Items & Electronic Devices</h3>
         </div>
         <div className={styles.container}>
-            <img className={styles.imgCar} src={audi} alt="" />
-            <img className={styles.imgCar} src={benz2} alt="" />
-            <img className={styles.imgCar} src={clio} alt="" />
+            <img className={styles.img} src={kit} alt="" />
+            <img className={styles.img} src={kit2} alt="" />
+            <img className={styles.img} src={kit3} alt="" />
         </div>
-        <footer className={styles.footer}>Images by: Mike Birdy on Pexels.</footer>
+        <footer className={styles.footer}>Images by: FOX1004, No Revisions, Keagan Henman & Lum3n on Pexels.</footer>
         </>
     
     )
