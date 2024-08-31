c27a497a0dd0bad2e4620558d5f62e464db74830
Mateus Felipe <brasil.mateus748@gmail.com>
2024-08-30 23:54:40 -0300

    Add useEffect hook to fetch products from an API

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Shop.module.css"
 import Card from "./Card/Card.js"
 import { useCart } from "../../context/CartContext";
 
 const Shop = () => {
+
     const { addToCart } = useCart();
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            try {
+                const response = await fetch('https://fakestoreapi.com/products');
+                const data = await response.json();
+                setItems(data);
+            } catch (error) {
+                console.error("Error fetching items:", error);
+            }
+        };
+
+        fetchItems();
+    }, []);
 
     const handleAddToCart = (item) => {
         addToCart(item);
@@ -14,7 +30,9 @@ const Shop = () => {
         <>
         <h1 className={styles.titleShop}>Shop Page</h1>
         <div className={styles.shopcontainer}>
-            <Card onAddToCart={handleAddToCart} />
+            {items.map((item) => (
+                    <Card key={item.id} item={item} onAddToCart={addToCart} />
+                ))}
         </div>
         </>
     
